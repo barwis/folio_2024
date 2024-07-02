5a3424bbae943e79634025bfda2ce42ee821c245
barwis <wisniewski.bart@gmail.com>
2024-07-02 20:57:04 +0100

    more wip

--- script2.js
+++ script2.js
@@ -11,6 +11,8 @@ gsap.ticker.add((time) => {
 
 gsap.ticker.lagSmoothing(0);
 
+const viewportHeight = window.innerHeight;
+
 function createRipple(event) {
     event.preventDefault();
     event.stopPropagation();
@@ -50,36 +52,39 @@ function createRipple(event) {
     }, 300);
 }
 
-function shuffleArray(array) {
-    let currentIndex = array.length,
-        temporaryValue,
-        randomIndex;
+const animateHeroSection = () => {
+    const elements = {
+        '#h-1': {
+            transform: 'translateX(-0.5em)',
+            opacity: 0,
+        },
+        '#h-2': {
+            transform: 'translateX(0.5em)',
+            opacity: 0,
+        },
+        '#h-tag': {
+            opacity: 0,
+        },
+    };
 
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+    const hero = document.querySelector('header');
+    if (!hero) return;
 
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-function* getRandomArrayItem(array) {
-    let index = Infinity;
-    const items = array.slice(); //take a copy of the array;
-
-    while (true) {
-        if (index >= array.length) {
-            shuffleArray(items);
-            index = 0;
+    Object.entries(elements).forEach(([id, styles]) => {
+        const elem = document.querySelector(id);
+        if (elem && hero) {
+            gsap.to(id, {
+                scrollTrigger: {
+                    scrub: true,
+                    start: 'top top',
+                    trigger: hero,
+                    end: () => `${viewportHeight}px`,
+                },
+                ...styles,
+            });
         }
-
-        yield items[index++];
-    }
-}
+    });
+};
 
 const randomiseHeaderText = () => {
     const texts = [
@@ -95,6 +100,38 @@ const randomiseHeaderText = () => {
         ['and I prefer spaces', 'over tabs', '( fight me! )'],
         ['and I turn caffeine', 'into websites', '( weird flex, but OK... )'],
     ];
+
+    function shuffleArray(array) {
+        let currentIndex = array.length,
+            temporaryValue,
+            randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+    function* getRandomArrayItem(array) {
+        let index = Infinity;
+        const items = array.slice(); //take a copy of the array;
+
+        while (true) {
+            if (index >= array.length) {
+                shuffleArray(items);
+                index = 0;
+            }
+
+            yield items[index++];
+        }
+    }
+
     // get elements
     const span1 = document.querySelector('#sh1');
     const span2 = document.querySelector('#sh2');
@@ -113,14 +150,17 @@ const randomiseHeaderText = () => {
     const randomText = getRandomArrayItem(texts);
 
     const animate = () => {
+        // clear elements' text
         span1.innerHTML = '';
         span2.innerHTML = '';
         span3.innerHTML = '';
 
         const textToShuffle = randomText.next().value;
+
         gsap.set(span3, { opacity: 0 });
         span3.innerHTML = textToShuffle[2] || '';
 
+        // calculate scramble dudation based on a length of the text
         const newDuration = parseFloat(
             textToShuffle.join('').length / 30
         ).toFixed(2);
@@ -159,5 +199,6 @@ if (mediaQuery.matches) {
 document.addEventListener('readystatechange', (event) => {
     if (event.target.readyState === 'complete') {
         randomiseHeaderText();
+        animateHeroSection();
     }
 });
